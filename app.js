f18b96d82e360f831f7c8c4e3d34879abdb26a2f
Cory Friedt <c.friedt89@gmail.com>
2020-04-26 16:17:49 -0700

    defined UI variables

---
+++ app.js
@@ -0,0 +1,12 @@
+// Define UI variables
+const submitBtn = document.getElementById('submitBtn'),
+      alert = document.getElementById('alert'),
+      textArea = document.getElementById('textArea'),
+      totalWords = document.getElementById('totalWords');
+
+
+
+// Event Handlers
+submitBtn.addEventListener('click', function(){
+  console.log('it works');
+});
